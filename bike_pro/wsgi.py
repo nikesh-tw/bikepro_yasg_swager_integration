9020d7ced3e19d4f10264e555553e177ba393275
nikesh-tw <nikesh.thoughtwin@gmail.com>
2022-07-13 17:00:51 +0530

    modify wsgi

--- bike_pro/wsgi.py
+++ bike_pro/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bike_pro.settings.dev')
 #application = DjangoWhiteNoise(get_wsgi_application())
